5d0aecbd09b5de8a3cda019ce171f0b9b0cc186d
Amanat Khan Shakil <amanatkhanshakil223@gmail.com>
2024-09-24 00:06:01 +0600

    Added functionality on Donate section

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ const donationList = document.getElementById("donation-list");
 const historyList = document.getElementById("history-list");
 const donationBtn = document.getElementById("donation-btn");
 const historyBtn = document.getElementById("history-btn");
+const cashAmount = document.getElementById("cash-amount");
 
 document.getElementById("donation-btn").addEventListener("click", function () {
   donationList.classList.remove("hidden");
@@ -15,4 +16,16 @@ document.getElementById("history-btn").addEventListener("click", function () {
   historyList.classList.remove("hidden");
   donationBtn.classList.remove("bg-primary");
   historyBtn.classList.add("bg-primary");
+});
+
+document.getElementById("post-1-btn").addEventListener("click", function () {
+  const postBtn = document.getElementById("post-1-btn");
+  const inputValue = handleInputValue("post-1-input");
+
+  const post1Amount = document.getElementById("post-1-amount").innerText;
+  const totalAmount = parseInt(post1Amount) + inputValue;
+  document.getElementById("post-1-amount").innerText = totalAmount;
+  cashAmount.innerText = parseInt(cashAmount.innerText) - inputValue;
+  handleHistoryList("post-1-title", inputValue);
+  document.getElementById("post-1-input").value = "";
 });
--- js/utilities.js
+++ js/utilities.js
@@ -1,1 +1,20 @@
-function handleButtonNavigation(id) {}
+function handleInputValue(id) {
+  const inputValue = document.getElementById(id).value;
+  const inputValueNumber = parseInt(inputValue);
+  return inputValueNumber;
+}
+
+function handleHistoryList(id, inputValue) {
+  const title = document.getElementById(id).innerText;
+  const historyList = document.getElementById("history-list");
+  historyList.innerHTML += `
+     <div class="p-8 border border-[#111111]/10 rounded-2xl mb-8">
+          <h2 class="text-xl font-bold mb-3">
+            ${inputValue} Taka is Donated for ${title}
+          </h2>
+          <p class="text-[#111111]/70 font-light ]">
+            Date : Tue Sep 17 2024 08:39:11 GMT +0600 (Bangladesh Standard Time)
+          </p>
+    </div>
+  `;
+}
