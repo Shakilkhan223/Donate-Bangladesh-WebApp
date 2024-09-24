edd50f7d1c1b4f962794f273664c28f8563e6435
Amanat Khan Shakil <amanatkhanshakil223@gmail.com>
2024-09-24 20:44:20 +0600

    Completed Home page and blog page with all the funcanalitty

--- js/app.js
+++ js/app.js
@@ -18,6 +18,7 @@ document.getElementById("history-btn").addEventListener("click", function () {
   historyBtn.classList.add("bg-primary");
 });
 
+// Donation post 01
 document.getElementById("post-1-btn").addEventListener("click", function () {
   const cashAmountNumber = parseInt(cashAmount.innerText);
   const inputFirstValue = document.getElementById("post-1-input");
@@ -42,5 +43,61 @@ document.getElementById("post-1-btn").addEventListener("click", function () {
   document.getElementById("post-1-amount").innerText = totalAmount;
   cashAmount.innerText = parseInt(cashAmount.innerText) - inputValue;
   handleHistoryList("post-1-title", inputValue);
+  inputFirstValue.value = "";
+});
+
+// Donation post 02
+document.getElementById("post-2-btn").addEventListener("click", function () {
+  const cashAmountNumber = parseInt(cashAmount.innerText);
+  const inputFirstValue = document.getElementById("post-2-input");
+  const inputValue = handleInputValue("post-2-input");
+
+  if (inputValue > cashAmountNumber) {
+    alert("You don't have enough cash");
+    inputFirstValue.value = "";
+    return;
+  }
+
+  if (isNaN(inputValue) || inputValue <= 0) {
+    alert("Please provide a valid number");
+    inputFirstValue.value = "";
+    return;
+  } else {
+    handleModal();
+  }
+
+  const post2Amount = document.getElementById("post-2-amount").innerText;
+  const totalAmount = parseInt(post2Amount) + inputValue;
+  document.getElementById("post-2-amount").innerText = totalAmount;
+  cashAmount.innerText = parseInt(cashAmount.innerText) - inputValue;
+  handleHistoryList("post-2-title", inputValue);
+  inputFirstValue.value = "";
+});
+
+// Donation post 03
+document.getElementById("post-3-btn").addEventListener("click", function () {
+  const cashAmountNumber = parseInt(cashAmount.innerText);
+  const inputFirstValue = document.getElementById("post-3-input");
+  const inputValue = handleInputValue("post-3-input");
+
+  if (inputValue > cashAmountNumber) {
+    alert("You don't have enough cash");
+    inputFirstValue.value = "";
+    return;
+  }
+
+  if (isNaN(inputValue) || inputValue <= 0) {
+    alert("Please provide a valid number");
+    inputFirstValue.value = "";
+    return;
+  } else {
+    handleModal();
+  }
+
+  const post3Amount = document.getElementById("post-3-amount").innerText;
+  const totalAmount = parseInt(post3Amount) + inputValue;
+  document.getElementById("post-3-amount").innerText = totalAmount;
+  cashAmount.innerText = parseInt(cashAmount.innerText) - inputValue;
+  handleHistoryList("post-3-title", inputValue);
   inputFirstValue.value = "";
 });
